2cf72a4f4b0409f88c9edb6dd8f8537bb549a9dc
Avi Bryant <avi@avibryant.com>
2013-04-26 11:18:06 -0700

    The most naive possible Cascading scheme that converts parquet columns to tuple fields

---
+++ parquet-cascading/src/main/java/parquet/cascading/ParquetTupleScheme.java
@@ -0,0 +1,82 @@
+ package parquet.cascading;
+
+ import java.io.IOException;
+
+ import org.apache.hadoop.mapred.JobConf;
+ import org.apache.hadoop.mapred.OutputCollector;
+ import org.apache.hadoop.mapred.RecordReader;
+
+ import parquet.example.data.simple.SimpleGroup;
+ import parquet.hadoop.example.ExampleInputFormat;
+ import parquet.hadoop.thrift.DeprecatedContainerInputFormat;
+ import parquet.hadoop.thrift.Container;
+
+ import cascading.flow.FlowProcess;
+ import cascading.scheme.SinkCall;
+ import cascading.scheme.Scheme;
+ import cascading.scheme.SourceCall;
+ import cascading.tap.Tap;
+ import cascading.tuple.Tuple;
+ import cascading.tuple.Fields;
+
+
+public class ParquetTupleScheme extends Scheme<JobConf, RecordReader, OutputCollector, Object[], Object[]>{
+
+  private static final long serialVersionUID = 0L;
+
+  public ParquetTupleScheme(Fields sourceFields) {
+    super(sourceFields);
+  }
+
+  @SuppressWarnings("rawtypes")
+  @Override
+  public void sourceConfInit(FlowProcess<JobConf> fp,
+      Tap<JobConf, RecordReader, OutputCollector> tap, JobConf jobConf) {
+    DeprecatedContainerInputFormat.setInputFormat(ExampleInputFormat.class, jobConf);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public boolean source(FlowProcess<JobConf> fp, SourceCall<Object[], RecordReader> sc)
+      throws IOException {
+    Container<SimpleGroup> value = (Container<SimpleGroup>) sc.getInput().createValue();
+    boolean hasNext = sc.getInput().next(null, value);
+    if (!hasNext) { return false; }
+
+    // Skip nulls
+    if (value == null) { return true; }
+
+    SimpleGroup group = value.get();
+    try {
+      Tuple tuple = new Tuple();
+      for(Comparable f : getSourceFields()) {
+        String fieldName = f.toString();
+        int index = group.getType().getFieldIndex(fieldName);
+        tuple.add(group.getValueToString(index, 0));
+      }
+      sc.getIncomingEntry().setTuple(tuple);
+    } catch (Exception e) {
+      System.out.println("Caught " + e + " when processing " + group);
+    }
+    return true;
+  }
+
+
+  @SuppressWarnings("rawtypes")
+  @Override
+  public void sinkConfInit(FlowProcess<JobConf> arg0,
+      Tap<JobConf, RecordReader, OutputCollector> arg1, JobConf arg2) {
+    throw new UnsupportedOperationException("ParquetTupleScheme does not support Sinks");
+
+  }
+
+  @Override
+  public boolean isSink() { return false; }
+
+
+  @Override
+  public void sink(FlowProcess<JobConf> arg0, SinkCall<Object[], OutputCollector> arg1)
+      throws IOException {
+    throw new UnsupportedOperationException("ParquetTupleScheme does not support Sinks");
+  }
+}
